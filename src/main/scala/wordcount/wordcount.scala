6cfff0433cab7984b17a84dbf7a6e21301234fe1
eren <eozel44@gmail.com>
2020-08-24 13:23:32 +0300

    wordcount sparksession refactoring

--- src/main/scala/wordcount/wordcount.scala
+++ src/main/scala/wordcount/wordcount.scala
@@ -1,18 +1,24 @@
 package wordcount
 
+import org.apache.spark.sql.SparkSession
 import org.apache.spark.{SparkConf, SparkContext}
+
 
 object wordcount {
 
   def main(args: Array[String]): Unit = {
 
-    val conf = new SparkConf().setAppName("wordcount").setMaster("local[2]")
-    val sc = new SparkContext(conf)
+    val spark = SparkSession
+      .builder
+      .appName("MnMCount")
+      .master("local[2]")
+      .getOrCreate()
 
+    spark.sparkContext
     val file = s"src/main/resources/wordcount/wordcount.txt"
 
     //base rdd
-    val lines = sc.textFile(file, 2)
+    val lines = spark.sparkContext.textFile(file, 2)
 
     //transformed rdd
     val words = lines.flatMap(line => line.split(" "))
@@ -26,7 +32,7 @@ object wordcount {
     val countHello = words.filter(word => word.contains("soul")).count
     println(s"word count= $countHello")
 
-    sc.stop()
+    spark.stop()
 
   }
 
