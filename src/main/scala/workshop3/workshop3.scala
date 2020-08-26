d73b8aa3c2eab282d4a96872293979ba5d7d7ed1
eren <eozel44@gmail.com>
2020-08-26 18:58:08 +0300

    comments added

--- src/main/scala/workshop3/workshop3.scala
+++ src/main/scala/workshop3/workshop3.scala
@@ -5,7 +5,38 @@ import org.apache.spark.sql.types._
 import org.apache.spark.sql.functions.{col, expr}
 
 /***
- * Dataframe
+ *
+ * RDD;
+ *   Dependencies
+ *   Partitions (with some locality information)
+ *   Compute function: Partition => Iterator[T]
+ *
+ * All three are integral to the simple RDD programming API model upon which all
+ * higher-level functionality is constructed.
+ * First, a list of dependencies that instructs Spark how an RDD is constructed with its inputs is required.
+ * When necessary to reproduce results, Spark can recreate an RDD from these dependencies and replicate operations on it.
+ * This characteristic gives RDDs resiliency.
+ *
+ * Second, partitions provide Spark the ability to split the work to parallelize computation on partitions across executors. In some cases—for example, reading from
+ * HDFS—Spark will use locality information to send work to executors close to the data.
+ * That way less data is transmitted over the network.
+ *
+ * And finally, an RDD has a compute function that produces an Iterator[T] for the
+ * data that will be stored in the RDD.
+ *
+ * DataFrame
+ *
+ * Spark DataFrames are like distributed in-memory tables with named columns and schemas,
+ * where each column has a specific data type: integer, string, array, map, real, date, timestamp, etc.
+ * To a human’s eye, a Spark DataFrame is like a table.  structured table!!!
+ *
+ * A schema in Spark defines the column names and associated data types for a DataFrame.
+ * Most often, schemas come into play when you are reading structured data from an external data source
+ *
+ *      You relieve Spark from the onus of inferring data types.
+ *      You prevent Spark from creating a separate job just to read a large portion of
+ *          your file to ascertain the schema, which for a large data file can be expensive and time-consuming.
+ *      You can detect errors early if data doesn’t match the schema.
  *
  *
  *
--- src/main/scala/workshop4/workshop4.scala
+++ src/main/scala/workshop4/workshop4.scala
@@ -4,6 +4,40 @@ import org.apache.spark.sql.SparkSession
 
 /***
  * Dataset
+ * Conceptually, you can think of a DataFrame in Scala as an alias for a collection of generic objects, Dataset[Row],
+ * where a Row is a generic untyped JVM object that may hold different types of fields.
+ * A Dataset, by contrast, is a collection of strongly typed JVM objects in Scala or a class in Java.
+ * A Dataset is;
+ *      a strongly typed collection of domain-specific objects that can be transformed in parallel
+ *      using functional or relational operations. Each Dataset [in Scala] also has an untyped view called a DataFrame,
+ *      which is a Dataset of Row.
+ *
+ */
+/***
+ * DataFrames Versus Datasets
+ *
+ *  If you want to tell Spark what to do, not how to do it, use DataFrames or Datasets.
+ *  If you want rich semantics, high-level abstractions, and DSL operators, use DataFrames or Datasets.
+ *  If you want strict compile-time type safety and don’t mind creating
+ *     multiple case classes for a specific Dataset[T], use Datasets.
+ *  If your processing demands high-level expressions, filters, maps, aggregations,computing averages or sums,
+ *    SQL queries, columnar access, or use of relational operators on semi-structured data, use DataFrames or Datasets.
+ *  If your processing dictates relational transformations similar to SQL-like queries, use DataFrames.
+ *  If you want to take advantage of and benefit from Tungsten’s efficient serialization with Encoders, use Datasets.
+ *  If you want unification, code optimization, and simplification of APIs across Spark components, use DataFrames.
+ *  If you are an R user, use DataFrames.
+ *  If you are a Python user, use DataFrames and drop down to RDDs if you need more control.
+ *  If you want space and speed efficiency, use DataFrames.
+ *
+ */
+
+/***
+ * When to Use RDD ?
+ *
+ *  Are using a third-party package that’s written using RDDs
+ *  Can forgo the code optimization, efficient space utilization,
+ *    and performance benefits available with DataFrames and Datasets
+ *  Want to precisely instruct Spark how to do a query
  *
  */
 
